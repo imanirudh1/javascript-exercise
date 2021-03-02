a5cae1fb0627df27ab1d366759bcdfb5701108c9
Aniruddha  Banerjee <aniruddha.banerjee@mountblue.tech>
2021-03-02 19:23:48 +0530

    this keyword demo

--- this-demo/index.js
+++ this-demo/index.js
@@ -1,13 +1,52 @@
-// function getBaseSpeed() {
-//   console.log('Base Speed Stat is : ', this.baseSpeed)
-// }
+// // function getBaseSpeed() {
+// //   console.log('Base Speed Stat is : ', this.baseSpeed)
+// // }
 
-// const pikachu = {
-//   baseSpeed: 90,
-//   getBaseSpeed: getBaseSpeed,
-// }
+// // const pikachu = {
+// //   baseSpeed: 90,
+// //   getBaseSpeed: getBaseSpeed,
+// // }
 
-// pikachu.getBaseSpeed()
+// // pikachu.getBaseSpeed()
+
+// // function getBaseSpeed() {
+// //   console.log('Base Speed Stat is : ', this.baseSpeed)
+// // }
+
+// // var pikachu = {
+// //   baseSpeed: 90,
+// //   getBaseSpeed: getBaseSpeed,
+// // }
+
+// // var raichu = {
+// //   baseSpeed: 110,
+// //   pikachu: pikachu,
+// // }
+
+// // raichu.pikachu.getBaseSpeed()
+
+// // const array1 = [1, 4, 9, 16]
+// // const ar = [1, 2, 3]
+// // // pass a function to map
+// // const map1 = array1.map((x, index, arr) => {
+// //   console.log('array1', array1)
+// //     console.log(arr)
+
+// // })
+
+// // console.log(map1)
+// // chaining of functions
+// const source = [-3, -2, -1, 0, 1, 2, 3, 4, 5]
+// source
+//   .filter((n) => n >= 0)
+//   .map((n, index, arr) => {
+//     console.log(arr)
+//     console.log('src', source)
+//     // arr contains only non-negative numbers
+//     // here you may have some logic that rely on it
+//     return n
+//   })
+// console.log(source)
 
 // function getBaseSpeed() {
 //   console.log('Base Speed Stat is : ', this.baseSpeed)
@@ -18,47 +57,22 @@
 //   getBaseSpeed: getBaseSpeed,
 // }
 
-// var raichu = {
-//   baseSpeed: 110,
-//   pikachu: pikachu,
-// }
+// var baseSpeedFunction = pikachu.getBaseSpeed
+// var baseSpeed = 50
+// baseSpeedFunction()
+// // Output
+// // Base Speed Stat is : 50
 
-// raichu.pikachu.getBaseSpeed()
-
-// const array1 = [1, 4, 9, 16]
-// const ar = [1, 2, 3]
-// // pass a function to map
-// const map1 = array1.map((x, index, arr) => {
-//   console.log('array1', array1)
-//     console.log(arr)
-
-// })
-
-// console.log(map1)
-// chaining of functions
-const source = [-3, -2, -1, 0, 1, 2, 3, 4, 5]
-source
-  .filter((n) => n >= 0)
-  .map((n, index, arr) => {
-    console.log(arr)
-    console.log('src', source)
-    // arr contains only non-negative numbers
-    // here you may have some logic that rely on it
-    return n
-  })
-console.log(source)
-
-function getBaseSpeed() {
-  console.log('Base Speed Stat is : ', this.baseSpeed)
+const obj = {
+  name: 'anirudh',
+  myname() {
+    console.log(this)
+  },
 }
+const a = obj.myname()
 
-var pikachu = {
-  baseSpeed: 90,
-  getBaseSpeed: getBaseSpeed,
+function global() {
+  this.a = 'an'
+  console.log(this)
 }
-
-var baseSpeedFunction = pikachu.getBaseSpeed
-var baseSpeed = 50
-baseSpeedFunction()
-// Output
-// Base Speed Stat is : 50
+const newF = new global()
